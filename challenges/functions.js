397d97f7c994ec5af067ce84532ea3b419035a26
weng7533 <weng7533@users.noreply.github.com>
2019-10-18 12:32:06 -0400

    Task 2: Functions done

--- challenges/functions.js
+++ challenges/functions.js
@@ -2,31 +2,53 @@
 
 /* Step 1: Create a higher-order function
   * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
+  * The first two parameters can take any argument
+  *  (we can pass any value as argument)
   * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
+  * The consume function should return the invocation of cb, 
+  * passing a and b into cb as arguments
 */
+
+function consume (a,b,cb){
+    return cb(a,b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * 
+  * Create a function named greeting that accepts
+  *  a first and last name and returns
+  *  "Hello first-name last-name, nice to meet you!"
 */
+function add(a,b){
+  return a+b;
+}
 
+function multiply(a,b){
+  return a*b;
+}
 
+function greeting(a,b){
+  return `Hello ${a} ${b}, nice to meet you!`;
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
 
 
 // ==== Closures ==== 
 
-// Explain in your own words why nestedfunction can access the variable internal.
+// Explain in your own words why nestedfunction
+// can access the variable internal.
 
 // Explanation: 
 
+console.log(`variable internal still inside of nestedFunction's scrope, in another word,
+within it's closure, therefor, it can still access internal`)
 
 const external = "I'm outside the function";
 
